9c5089b832ade232578238777590ae3957c441dc
flodavis <fpdavis86@gmail.com>
2019-03-26 19:26:36 -0400

    added basic controllers for remaining requests put and delete

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Put, Delete, Body } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
 import { CreateItemDto} from './dto/create-item.dto';
 
 @Controller('items')
@@ -7,8 +7,20 @@ export class ItemsController {
     findAll() : string {
         return 'Get all items';
     }
+    @Get(':id')
+    findOne(@Param() param): string {
+        return `Item ${param.id}`;
+    }
     @Post()
     create(@Body() CreateItemDto: CreateItemDto): string {
         return `Name: ${CreateItemDto.name} Desc: ${CreateItemDto.description} Qty: ${CreateItemDto.qty}`;
+    }
+    @Delete(':id')
+    delete(@Param('id') id): string{
+        return `delete ${id}`;
+    }
+    @Put(':id')
+    update(@Body() updateItemDto: CreateItemDto, @Param('id') id): string{
+        return `Update ${id} - Name: ${updateItemDto.name}`;
     }
 }
