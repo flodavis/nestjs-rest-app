6ce83d661f1c38130d391bf039b6feecc6c61dc8
flodavis <fpdavis86@gmail.com>
2019-03-26 20:19:08 -0400

    implemented MongoDB, and mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsController } from './items/items.controller';
+import { ItemsModule } from './items/items.module';
 import { ItemsService } from './items/items.service';
-
+import config from './config/keys'
 @Module({
-  imports: [],
+  imports: [ItemsModule, MongooseModule.forRoot(config.mongoURI)],
   controllers: [AppController, ItemsController],
   providers: [AppService, ItemsService],
 })
---
+++ src/config/keys.ts
@@ -0,0 +1,3 @@
+export default {
+    mongoURI: 'mongodb+srv://flow:Pat17ricia!@cluster0-cximt.gcp.mongodb.net/test?retryWrites=true',
+};
